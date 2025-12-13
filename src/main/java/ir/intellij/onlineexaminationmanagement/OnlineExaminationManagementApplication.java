80ef7fd62910fb09e237a29f1df0169de356b2d7
Davoud <sheikhidavoud@gmail.com>
2025-12-13 17:55:10 +0330

    build: add pom.xml and maven config

---
+++ src/main/java/ir/intellij/onlineexaminationmanagement/OnlineExaminationManagementApplication.java
@@ -0,0 +1,13 @@
+package ir.intellij.onlineexaminationmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineExaminationManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineExaminationManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/intellij/onlineexaminationmanagement/OnlineExaminationManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.intellij.onlineexaminationmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineExaminationManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
