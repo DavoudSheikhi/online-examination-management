0b2be6629e5b241dc06ea12ce5760836ae90862e
Davoud <sheikhidavoud@gmail.com>
2025-12-15 22:58:15 +0330

    update entity: BaseModel

--- src/main/java/ir/intellij/onlineexaminationmanagement/model/BaseModel.java
+++ src/main/java/ir/intellij/onlineexaminationmanagement/model/BaseModel.java
@@ -3,20 +3,10 @@ package ir.intellij.onlineexaminationmanagement.model;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.experimental.SuperBuilder;
 
 import java.time.Instant;
 
-@Getter
-@Setter
 @MappedSuperclass
-@SuperBuilder
-@AllArgsConstructor
-@NoArgsConstructor
 
 public class BaseModel {
 
