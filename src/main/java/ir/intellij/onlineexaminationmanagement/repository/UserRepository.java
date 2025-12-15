6830a9dbc9bee3c49774f4a4d87efc5ee620c08c
Davoud <sheikhidavoud@gmail.com>
2025-12-15 19:31:54 +0330

    feat: add repo & Service for User

---
+++ src/main/java/ir/intellij/onlineexaminationmanagement/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package ir.intellij.onlineexaminationmanagement.repository;
+
+import ir.intellij.onlineexaminationmanagement.model.Role;
+import ir.intellij.onlineexaminationmanagement.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    List<User> findUserByRole(Role role);
+}
---
+++ src/main/java/ir/intellij/onlineexaminationmanagement/service/UserService.java
@@ -0,0 +1,7 @@
+package ir.intellij.onlineexaminationmanagement.service;
+
+import ir.intellij.onlineexaminationmanagement.model.User;
+
+public interface UserService {
+    User save(User user);
+}
