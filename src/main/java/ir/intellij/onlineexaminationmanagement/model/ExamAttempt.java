4c16511c9712be879d95d77a2966f7e7ac91d22a
Davoud <sheikhidavoud@gmail.com>
2026-02-15 13:14:03 +0330

    feat(model): add ExamAttempt class

---
+++ src/main/java/ir/intellij/onlineexaminationmanagement/model/ExamAttempt.java
@@ -0,0 +1,51 @@
+package ir.intellij.onlineexaminationmanagement.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"exam_id", "student_id"})
+        }
+)
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+public class ExamAttempt extends BaseModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "attemptExam_seq")
+    @SequenceGenerator(name = "attemptExam_seq", sequenceName = "attemptExam_seq", allocationSize = 5)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private Exam exam;
+
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private User student;
+
+    private LocalDateTime startedAt;
+    private LocalDateTime endsAt;
+    private LocalDateTime submittedAt;
+
+    @Enumerated(EnumType.STRING)
+    private ExamAttemptStatus status;
+
+    @Column(nullable = false)
+    private Double totalAutoScore = 0d;
+
+    @Column(nullable = false)
+    private Double totalManualScore = 0d;
+
+    @Column(nullable = false)
+    private Double totalScore = 0d;
+
+}
