4d37659e263bed5b9e87dc7372a5e7ff4f80c04d
Davoud <sheikhidavoud@gmail.com>
2026-02-21 14:19:17 +0330

    feat(repo): refactor AttemptAnswerRepository

--- src/main/java/ir/intellij/onlineexaminationmanagement/repository/AttemptAnswerRepository.java
+++ src/main/java/ir/intellij/onlineexaminationmanagement/repository/AttemptAnswerRepository.java
@@ -2,7 +2,20 @@ package ir.intellij.onlineexaminationmanagement.repository;
 
 import ir.intellij.onlineexaminationmanagement.model.AttemptAnswer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface AttemptAnswerRepository extends JpaRepository<AttemptAnswer, Long> {
 
+    @Query("""
+            select aa
+            from AttemptAnswer aa
+            join fetch aa.examQuestion q
+            left join fetch aa.selectedExamOption seo
+            where aa.attempt.id = :attemptId
+            order by q.id asc
+            """)
+    List<AttemptAnswer> findAllByAttemptIdWithQuestion(@Param("attemptId") Long attemptId);
 }
